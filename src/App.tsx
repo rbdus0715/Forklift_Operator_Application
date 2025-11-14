b5d649ad7972894cd99eb7ca2b7e2c378df6cdf9
rbdus0715 <rbdus0715@gmail.com>
2025-11-14 16:05:18 +0900

    Chore: typescript settings

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default App;
