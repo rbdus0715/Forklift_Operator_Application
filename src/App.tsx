eb2f2a06e710349d4f66003444d43509122c293c
rbdus0715 <rbdus0715@gmail.com>
2025-11-19 20:09:45 +0900

    Feat: 기본 화면 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import { Text, View } from "react-native";
 
 export const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
       <Text>Hello, World!</Text>
     </View>
   );
